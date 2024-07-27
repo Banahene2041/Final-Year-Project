bee5c6b5ccba1b099538121e502e6059c444263c
Banahene2041 <0542342728c@gmail.com>
2024-07-27 05:51:59 -1200

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import SignInPage from './pages/SignUpPage'
 import LoginPage from './pages/LoginPage'
+import LandingPage from './pages/LandingPage'
 
 // pages
 
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <>
     <Routes>
+      <Route path='/' element={<LandingPage />} />
       <Route path='/signup' element={<SignInPage />} />
       <Route path='/login' element={<LoginPage />} />
     </Routes>
